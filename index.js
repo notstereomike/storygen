24d593545b792b776a002b14274d63b16cdcec3f
notstereomike <51369046+notstereomike@users.noreply.github.com>
2023-03-24 15:31:23 +0000

    Added Image Generation.

--- index.js
+++ index.js
@@ -18,29 +18,40 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/generate', (req, res) => {
-    const { gender, race, classType } = req.body;
+app.post('/generate', async (req, res) => {
+    const { gender, race, classType, generateImage } = req.body;
 
     const params = {
         "prompt": `Write complete and concise 300 word backstory for a fictional D&D Charecter who is a ${gender} ${race} ${classType} character.`, 
-        max_tokens: 300,
+        max_tokens: 350,
         temperature: 0.7,
         top_p: 1,
-  frequency_penalty: 0,
-  presence_penalty: 0,
-        
-        
-        
-    }
+        frequency_penalty: 0,
+        presence_penalty: 0,      
+    };
 
-    client.post('https://api.openai.com/v1/engines/text-davinci-002/completions', params)
-        .then(result => {
-            const story = result.data.choices[0].text;
-            res.send(story);
-        }).catch(err => {
-            console.log(err);
-            res.send('Error generating story');
-        });
+   try {
+    const storyResult = await client.post('https://api.openai.com/v1/engines/text-davinci-002/completions', params);
+    const story = storyResult.data.choices[0].text;
+
+    if (generateImage === 'on') {
+      const imagePrompt = `A ${gender} ${race} ${classType}. Epic, Fantasy Art, Sharp, 4K`;
+
+      const imageResponse = await client.post('https://api.openai.com/v1/images/generations', {
+        prompt: imagePrompt,
+        n: 1,
+        size: '256x256',
+      });
+
+      const imageUrl = imageResponse.data.data[0].url;
+      res.send({ story, imageUrl });
+    } else {
+      res.send({ story });
+    }
+  } catch (err) {
+    console.log(err);
+    res.send({ error: 'Error generating story' });
+  }
 });
 
 app.listen(port, () => {
