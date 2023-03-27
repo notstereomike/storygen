cde7c72907d5e7237940ba9445db00459b9e1b0c
notstereomike <51369046+notstereomike@users.noreply.github.com>
2023-03-27 11:44:53 +0000

    Added save function for images and text

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const apiKey = process.env.OPENAI_API_KEY;
 const client = axios.create({
-    headers: { 'Authorization': 'Bearer ' + apiKey }
+  headers: { 'Authorization': 'Bearer ' + apiKey }
 });
 
 const app = express();
@@ -15,24 +16,29 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/generate', async (req, res) => {
-    const { gender, race, classType, generateImage } = req.body;
+  const { gender, race, classType, generateImage } = req.body;
 
-    const params = {
-        "prompt": `Write complete and concise 300 word backstory for a fictional D&D Charecter who is a ${gender} ${race} ${classType} character.`, 
-        max_tokens: 350,
-        temperature: 0.7,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,      
-    };
+  const params = {
+    "prompt": `Write complete and concise 300 word backstory for a fictional D&D Charecter who is a ${gender} ${race} ${classType} character.`,
+    max_tokens: 350,
+    temperature: 0.7,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  };
 
-   try {
+  try {
     const storyResult = await client.post('https://api.openai.com/v1/engines/text-davinci-002/completions', params);
     const story = storyResult.data.choices[0].text;
+
+    // Save the story to a file
+    const timestamp = new Date().toISOString().replace(/[-:.]/g, "");
+    const storyFileName = `output/story_${timestamp}.txt`;
+    fs.writeFileSync(storyFileName, story);
 
     if (generateImage === 'on') {
       const imagePrompt = `A ${gender} ${race} ${classType}. Epic, Fantasy Art, Sharp, 4K`;
@@ -44,6 +50,12 @@ app.post('/generate', async (req, res) => {
       });
 
       const imageUrl = imageResponse.data.data[0].url;
+      const imageBuffer = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+
+      // Save the image to a file
+      const imageFileName = `output/image_${timestamp}.png`;
+      fs.writeFileSync(imageFileName, Buffer.from(imageBuffer.data), 'binary');
+
       res.send({ story, imageUrl });
     } else {
       res.send({ story });
@@ -55,5 +67,5 @@ app.post('/generate', async (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`)
+  console.log(`App listening at http://localhost:${port}`)
 });
